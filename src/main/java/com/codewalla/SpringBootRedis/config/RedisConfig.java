ab6b18f1f7f95d6bc4bc629736f2e429091e5eac
gaurav-kj <gaurav.jaiswal@codewalla.com>
2021-09-15 23:18:06 +0530

    Import Optimization

--- src/main/java/com/codewalla/SpringBootRedis/config/RedisConfig.java
+++ src/main/java/com/codewalla/SpringBootRedis/config/RedisConfig.java
@@ -2,19 +2,13 @@ package com.codewalla.SpringBootRedis.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
 
 @Component
