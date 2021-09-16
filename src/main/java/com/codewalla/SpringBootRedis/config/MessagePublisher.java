59513c322067fdebbbd8006d1d5352831d214dde
gaurav-kj <gaurav.jaiswal@codewalla.com>
2021-09-16 11:18:35 +0530

    Removed PUB-SUB ,

--- src/main/java/com/codewalla/SpringBootRedis/config/MessagePublisher.java
+++
@@ -1,6 +0,0 @@
-package com.codewalla.SpringBootRedis.config;
-
-
-public interface MessagePublisher {
-    void publish(String message);
-}
--- src/main/java/com/codewalla/SpringBootRedis/config/RedisConfig.java
+++ src/main/java/com/codewalla/SpringBootRedis/config/RedisConfig.java
@@ -5,9 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.stereotype.Component;
 
@@ -19,24 +16,6 @@ public class RedisConfig {
 
 
     @Bean
-    public RedisMessageListenerContainer container(RedisConnectionFactory redisConnectionFactory, MessageListenerAdapter messageListenerAdapter){
-        RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer();
-        redisMessageListenerContainer.setConnectionFactory(redisConnectionFactory);
-        redisMessageListenerContainer.addMessageListener(messageListenerAdapter,topic());
-        return redisMessageListenerContainer;
-    }
-
-    @Bean
-    MessageListenerAdapter messageListenerAdapter()
-    {
-        return new MessageListenerAdapter(new RedisMessageSubscriber(),"onMessage");
-    }
-    @Bean
-    public ChannelTopic topic() {
-        return new ChannelTopic("Redis_Spring");
-    }
-
-    @Bean
     RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
     {
         RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
@@ -45,9 +24,5 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    @Bean
-    MessagePublisher messagePublisher()
-    {
-        return new RedisMessagePublisher(redisTemplate(redisConnectionFactory),topic());
-    }
+
 }
